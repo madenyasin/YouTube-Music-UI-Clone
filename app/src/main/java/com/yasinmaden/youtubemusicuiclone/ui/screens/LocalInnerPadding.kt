b57fbb5a52a26fb1efdff307f5f7345d7265bbe7
Yasin Maden <maden.yasin@hotmail.com>
2024-08-29 14:22:56 +0300

    FilterChipButton ve LazyRow tamamlandı.

---
+++ app/src/main/java/com/yasinmaden/youtubemusicuiclone/ui/screens/LocalInnerPadding.kt
@@ -0,0 +1,6 @@
+package com.yasinmaden.youtubemusicuiclone.ui.screens
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.compositionLocalOf
+
+val LocalInnerPadding = compositionLocalOf { PaddingValues() }
