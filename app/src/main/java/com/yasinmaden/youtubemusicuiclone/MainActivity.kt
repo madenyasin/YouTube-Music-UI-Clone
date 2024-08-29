b73678d1266c109600fadd08e0df236a0fe5473d
Yasin Maden <maden.yasin@hotmail.com>
2024-08-29 15:29:06 +0300

    ufak düzeltmeler.

--- app/src/main/java/com/yasinmaden/youtubemusicuiclone/MainActivity.kt
+++ app/src/main/java/com/yasinmaden/youtubemusicuiclone/MainActivity.kt
@@ -1,22 +1,21 @@
 package com.yasinmaden.youtubemusicuiclone
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
-import com.yasinmaden.youtubemusicuiclone.ui.navigation.NavGraph
 import com.yasinmaden.youtubemusicuiclone.ui.components.bottom_nav_bar.BottomBarNavigation
 import com.yasinmaden.youtubemusicuiclone.ui.components.top_app_bar.CustomTopAppBar
+import com.yasinmaden.youtubemusicuiclone.ui.navigation.NavGraph
 import com.yasinmaden.youtubemusicuiclone.ui.screens.LocalInnerPadding
 import com.yasinmaden.youtubemusicuiclone.ui.theme.YouTubeMusicUICloneTheme
 
@@ -42,6 +41,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Preview(showBackground = true)
 @Composable
 fun AppPreview() {
@@ -51,11 +51,10 @@ fun AppPreview() {
             bottomBar = { BottomBarNavigation(navController = navController) },
             topBar = { CustomTopAppBar() },
             modifier = Modifier.fillMaxSize()
-        ) { innerPadding ->
-            NavGraph(
-                navController = navController,
-                modifier = Modifier.padding(innerPadding)
-            )
+            ) { innerPadding ->
+            CompositionLocalProvider(LocalInnerPadding provides innerPadding) {
+                NavGraph(navController = navController, modifier = Modifier.padding(innerPadding))
+            }
         }
     }
 }
--- app/src/main/java/com/yasinmaden/youtubemusicuiclone/ui/components/top_app_bar/CustomTopAppBar.kt
+++ app/src/main/java/com/yasinmaden/youtubemusicuiclone/ui/components/top_app_bar/CustomTopAppBar.kt
@@ -3,25 +3,21 @@ package com.yasinmaden.youtubemusicuiclone.ui.components.top_app_bar
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.yasinmaden.youtubemusicuiclone.R
 
 
@@ -39,7 +35,7 @@ fun CustomTopAppBar() {
                 Image(
                     painter = painterResource(id = R.drawable.yt_music_logo),
                     contentDescription = null,
-                    modifier = Modifier.size(95.dp)
+                    modifier = Modifier.size(95.dp).padding(start = 5.dp)
                 )
                 Row (modifier = Modifier.width(150.dp), horizontalArrangement = Arrangement.SpaceEvenly){
 
